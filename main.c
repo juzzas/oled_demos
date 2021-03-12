c4aceb4637c6600dbd86c11e5ff50ccccf60b8ed
Justin Skists <justin.skists@juzza.co.uk>
2021-03-12 11:29:51 +0000

    added simple "sprite" placement

--- main.c
+++ main.c
@@ -6,6 +6,10 @@
 
 #include "oled.h"
 
+#include "jskists_sprite.h"
+#include "rc2014_sprite.h"
+#include "quazar_sprite.h"
+
 
 extern void pixel_test(void);
 #define OLED_IMAGE_SIZE  512
@@ -25,8 +29,30 @@ void set_image_buffer_random(void)
 }
 
 
+void put_sprite_rc(uint8_t *buffer, uint8_t *sprite, uint8_t row, uint8_t column, uint8_t h, uint8_t w)
+{
+    uint8_t x, y;
+    int index;
+
+    for (y = row; y < row + h; y++)
+    {
+        for (x = column; x < column + w; x++)
+        {
+            index = (y * 128) + x;
+            buffer[index] = *sprite;
+            sprite++;
+        }
+    }
+}
+
+
+
 int main(void)
 {
+    int sprite_index = 0;
+    uint8_t *sprite = NULL;
+    uint16_t counter = 0;
+    
     printf("Quazar OLED test\r\n");
     oled_init();
     printf("Quazar OLED done init\r\n");
@@ -35,22 +61,44 @@ int main(void)
 
     oled_blit_init();
     
-    memset(L_image_buffer, 0xa5, OLED_IMAGE_SIZE);
-    oled_blit(L_image_buffer);
-    
-    z80_delay_ms(250);
-    
-    oled_blit(0);
-
-    printf("Quazar OLED done test\r\n");
-    z80_delay_ms(250);
-    
+    printf("Quazar OLED test run...\r\n");
     
     while(1)
     {
+        if ((counter % 64 == 63))
+        {
+            switch (sprite_index)
+            {
+                case 0:
+                    sprite = jskists_bin;
+                    sprite_index++;
+                    break;
+                    
+                case 1:
+                    sprite = rc2014_bin;
+                    sprite_index++;
+                    break;
+                    
+                case 2:
+                default:
+                    sprite = quazar_bin;
+                    sprite_index = 0;
+                    break;
+            }
+        }
+            
         set_image_buffer_random();
+        
+        if (sprite)
+        {
+            put_sprite_rc(L_image_buffer, sprite, 1, 32, 2, 64);
+        }
+
         oled_blit(L_image_buffer);
+
         z80_delay_ms(25);
+        
+        counter++;
     }
         
     return 0;

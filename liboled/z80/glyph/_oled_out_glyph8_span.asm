a7bd38335286712058bc235d76e092fee5944a9d
Justin Skists <justin.skists@juzza.co.uk>
2021-08-06 14:42:02 +0100

    glyph8: span: factor out current use of the IY register

--- liboled/z80/glyph/_oled_out_glyph8_span.asm
+++ liboled/z80/glyph/_oled_out_glyph8_span.asm
@@ -1,4 +1,4 @@
-;; Output a glyph 8bits high (1 row) on a row boundary.
+;; Output a glyph 8bits high (1 row) over a row boundary.
 
 ;; Note: This routine does not check for overflow
 
@@ -18,10 +18,10 @@ PUBLIC oled_out_glyph8_span
 ;;        DE = incremented destination address
 
 oled_out_glyph8_span:
-        PUSH IY
-
-        ; store destination address
-        LD IY, DE
+        PUSH DE      ; store destination address in DE'
+        EXX
+        POP DE
+        EXX
 
 glyph_loop:
         ; loop for each width
@@ -47,14 +47,14 @@ data_shift_loop:
 
         ; entry: A = mask
         ;        B = shift
-        ;
-        ; exit:  HL = mask shifted by B bits, bits inverted
         LD H, 0
         LD L, A
 mask_shift_loop:
         ADD HL, HL
         DJNZ mask_shift_loop
+        ; exit:  HL = mask shifted by B bits, bits inverted
 
+        ; invert mask
         LD A, H
         CPL
         LD H, A
@@ -65,31 +65,26 @@ mask_shift_loop:
 
         PUSH HL      ; push mask on stack
 
-        LD HL, IY
-
 ;; load column span of two rows as 16bit value
         ; load the data from two rows 16-bit value. LSB is current address, MSB is address + 128
         ; DE = pointer to current pointer
         ;
-        ; exit: BC = 16 bit value
         LD A, (DE)
         LD C, A
         LD HL, OLED_WIDTH
         ADD HL, DE
         LD A, (HL)
         LD B, A
-        PUSH BC
+        ; exit: BC = 16 bit value
 
-        POP HL         ; restore current data
+        LD HL, BC      ; restore current data
         POP DE         ; restore mask
         POP BC         ; restore data
 
 ;; merge masked new data onto destination
         ; entry: HL = 16bit current data
         ;        DE = 16bit mask
-        ;        BC = 16 bit data
-        ;
-        ; exit:  HL = 16bit destination data
+        ;        BC = 16bit data
         LD A, H
         AND D
         OR B
@@ -99,23 +94,29 @@ mask_shift_loop:
         AND E
         OR C
         LD L, A
+        ; exit:  HL = 16bit destination data
 
 ; write data, and increment pointers
-        LD DE, IY
-        LD (IY+0), L
-        LD BC, OLED_WIDTH
-        ADD IY, BC
-        LD (IY+0), H
-        LD IY, DE
+        EXX       ; 4
+        PUSH DE   ; 11
+        PUSH DE   ; 11
+        INC DE    ; increment destination for next time
+        EXX       ; 4
+        POP DE    ; 10
 
-        INC IY
+        EX DE, HL
+        LD (HL), E
+        LD BC, OLED_WIDTH
+        ADD HL, BC
+        LD (HL), D
+        POP DE
+
         INC IX
 
         ; loop for next iteration
         POP BC   ; count
         DJNZ glyph_loop
 
-        LD DE, IY
+        EXX    ; let DE' be the exit condition
 
-        POP IY
         RET
